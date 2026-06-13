42d2a67021cc10689a8b03f1522485dec4f7bd41
habibsheikhh <ashrafsheiikh@example.com>
2026-06-14 01:18:41 +0530

    day3

--- Codeforces/Shayan_DP/478D.cpp
+++ Codeforces/Shayan_DP/478D.cpp
@@ -3,14 +3,38 @@ using namespace std;
 
 #define int long long
 
+const int MOD = 1e9 + 7;
+
 int32_t main() {
     ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
+    cin.tie(nullptr);
 
-    int r,g;
+    int r, g;
     cin >> r >> g;
 
-    
+    int total = r + g;
+
+    vector<int> dp(r + 1, 0);
+    dp[0] = 1;
+
+    int h = 1;
+    for(; h*(h+1)/2 <= total; h++) {
+        for(int red = r; red >= h; red--) {
+            dp[red] = (dp[red] + dp[red-h]) % MOD;
+        }
+    }
+    h--;
+    int need = h*(h+1) / 2;
+    int ans = 0;
+
+    for(int red = 0; red <= r; red++) {
+        int green = need - red;
+        if(green <= g &&  green >= 0) {
+            ans = (ans + dp[red]) % MOD;
+        }
+    }
+
+    cout << ans << '\n';
+
     return 0;
 }
--- templates/segment_tree.cpp
+++ templates/segment_tree.cpp
@@ -126,3 +126,76 @@ struct SegTree {
         else seg[i] = rightQ;
     }
 };
+
+
+// SegTree with Lazy Propagation
+
+struct SegTree {
+    int n;
+    vector<int> mn, mx, lazy;
+
+    SegTree(int n) {
+        this->n = n;
+        mn.assign(4 * n, 0);
+        mx.assign(4 * n, 0);
+        lazy.assign(4 * n, 0);
+    }
+
+    void propagate(int node, int l, int r) {
+        if (lazy[node] == 0) return;
+
+        mn[node] += lazy[node];
+        mx[node] += lazy[node];
+
+        if (l != r) {
+            lazy[2 * node + 1] += lazy[node];
+            lazy[2 * node + 2] += lazy[node];
+        }
+
+        lazy[node] = 0;
+    }
+
+    void update(int node, int l, int r, int ql, int qr, int val) {
+        propagate(node, l, r);
+
+        if (qr < l || r < ql) return;
+
+        if (ql <= l && r <= qr) {
+            lazy[node] += val;
+            propagate(node, l, r);
+            return;
+        }
+
+        int mid = (l + r) / 2;
+
+        update(2 * node + 1, l, mid, ql, qr, val);
+        update(2 * node + 2, mid + 1, r, ql, qr, val);
+
+        mn[node] = min(mn[2 * node + 1], mn[2 * node + 2]);
+        mx[node] = max(mx[2 * node + 1], mx[2 * node + 2]);
+    }
+
+    int findLeftZero(int node, int l, int r) {
+        propagate(node, l, r);
+
+        if (mn[node] > 0 || mx[node] < 0) return -1;
+
+        if (l == r) return l;
+
+        int mid = (l + r) / 2;
+
+        int left = findLeftZero(2 * node + 1, l, mid);
+        if (left != -1) return left;
+
+        return findLeftZero(2 * node + 2, mid + 1, r);
+    }
+
+    void update(int l, int r, int val) {
+        if (l > r) return;
+        update(0, 0, n - 1, l, r, val);
+    }
+
+    int findLeftZero() {
+        return findLeftZero(0, 0, n - 1);
+    }
+};
